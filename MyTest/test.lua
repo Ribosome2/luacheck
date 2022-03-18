98e1c68e5039dfcd0e031217b024d71279f5da26
Ribosome2 <583154233@qq.com>
2022-03-18 18:38:08 +0800

    分析parse的流程

--- MyTest/test.lua
+++ MyTest/test.lua
@@ -1,5 +1,10 @@
 
-local var =22
-function testFunction(argA,argB,argC)
-
+function testFunction(argA,argB)
+    return 111,233
 end
+
+function testFunction2(argA,argB,NN,kk)
+    return 111,233
+end
+
+testFunction(1,3,6,6)
