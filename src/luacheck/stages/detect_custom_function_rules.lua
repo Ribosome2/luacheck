185e7823df982d856eca0ffadd7e00fa86731809
Ribosome2 <583154233@qq.com>
2022-03-22 09:12:19 +0800

    some chstate is nil ,dont use it as local var

--- src/luacheck/stages/detect_custom_function_rules.lua
+++ src/luacheck/stages/detect_custom_function_rules.lua
@@ -6,7 +6,6 @@ stage.warnings = {
     ["800"] = {message_format = "function {name} has too many arguments ", fields = {'name'}},
     ["801"] = {message_format = "function {name} is too big: takes {lineCount} lines ", fields = {'name',"lineCount"}},
 }
-local _chstate
 local max_function_argument_count = 4
 local max_function_line_count = 80
 
@@ -19,15 +18,16 @@ if CUSTOM_MAX_FUNCTION_LINE_COUNT then
     max_function_line_count = CUSTOM_MAX_FUNCTION_LINE_COUNT
 end
 
-local function  check_single_function(node)
+local function  check_single_function(node,chstate)
+
     local args = node[1]
     if #args> max_function_argument_count then
-        _chstate:warn_range("800",node,{name=node.name})
+        chstate:warn_range("800",node,{name=node.name})
     end
     if node.end_line and node.line then
         local lineCount = node.end_line - node.line
         if lineCount> max_function_line_count then
-            _chstate:warn_range("801",node,{
+            chstate:warn_range("801",node,{
                 name=node.name,
                 lineCount=lineCount
             })
@@ -36,7 +36,7 @@ local function  check_single_function(node)
 end
 
 
-local function handle_nodes(nodes, list_start)
+local function handle_nodes(nodes, chstate)
     local num_nodes = #nodes
 
     for index = 1, num_nodes do
@@ -44,19 +44,17 @@ local function handle_nodes(nodes, list_start)
         if type(node) == "table" then
             local tag = node.tag
             if tag=="Function" then
-                check_single_function(node)
+                check_single_function(node,chstate)
             elseif tag == "Set" then
-                handle_nodes(node[1])
-                handle_nodes(node[2], 1)
+                handle_nodes(node[1],chstate)
+                handle_nodes(node[2], chstate)
             end
         end
     end
 end
 
--- Warns about assignments, field accesses, and mutations of global variables,
--- tracing through localizing assignments such as `local t = table`.
+
 function stage.run(chstate)
-    _chstate=chstate
-    handle_nodes(chstate.ast)
+    handle_nodes(chstate.ast,chstate)
 end
 return stage
